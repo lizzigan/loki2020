0ace841a9b8d967bcc3fc6b74cb32a4450f70057
lizzigan <44586334+lizzigan@users.noreply.github.com>
2020-01-14 14:48:07 -0500

    Add files via upload

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,106 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+// imported needed libraries to use to get buttons, joysticks, and put this on smartdashbaord
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj.smartdashboard.*;
+
+// importing our code so we can program the buttons to do certain commands
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.TestMotot;
+import frc.robot.commands.*;
+
+
+
+
+/**
+ * This class is where the bulk of the robot should be declared.  Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
+ * (including subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private final DriveTrain m_drive = new DriveTrain();
+  public Joystick xbox;
+  public Button a;
+  public Button b;
+  public JoystickButton lmove;
+  public JoystickButton rmove;
+
+  /**
+   * The container for the robot.  Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() 
+  {
+    // Configure the button bindings
+    xbox = new Joystick(1);
+    rmove = new JoystickButton(xbox,1);
+    lmove = new JoystickButton(xbox,5);
+   // configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings.  Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
+   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  //private void configureButtonBindings() {
+  
+ //   a.whenPressed(new Spin());
+
+    
+ // }
+
+  public double getXboxYSpeed()
+  {
+    // if the y axis is more pushed more than a certain amount then (to account for drivers accidentally
+    // pressing on buttons) then the raw axis will be returned. That value will then be used to drive 
+    // the robot. otherwise the it will return 0 and it will not move. 1 is the Y Axis on the xbox controller
+    if(Math.abs(xbox.getRawAxis(1)) >= .2)
+    {
+        return xbox.getRawAxis(1);
+    }
+    return 0.0;
+  }
+
+  public double getXboxXSpeed()
+  {
+    // if the x axis is more pushed more than a certain amount then (to account for drivers accidentally
+    // pressing on buttons) then the raw axis will be returned. That value will then be used to drive 
+    // the robot. otherwise the it will return 0 and it will not move. 0 is the x Axis on the xbox controller
+    if(Math.abs(xbox.getRawAxis(5)) >= .2)
+    {
+        return xbox.getRawAxis(5);
+    }
+    return -0.0;
+  }
+
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return m_autoCommand;
+  }
+}
---
+++ src/main/java/frc/robot/commands/Drive.java
@@ -0,0 +1,53 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.Constants;
+import frc.robot.Robot;
+import frc.robot.RobotContainer;
+
+public class Drive extends CommandBase {
+  /**
+   * Creates a new Drive.
+   */
+  double xs, ys, zs;
+  public Drive() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(Robot.m_drive);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() 
+  {
+    
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute()
+  {
+    xs = Robot.m_roboCon.getXboxXSpeed();
+    ys = Robot.m_roboCon.getXboxYSpeed();
+    Robot.m_drive.regDrive(xs, ys);
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
